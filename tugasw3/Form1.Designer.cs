3d716de68c8e728d466ed0ff0ae0c83afbf171b4
jessiline <jimanuela@student.ciputra.ac.id>
2022-03-12 21:10:19 +0700

    Add project files.

---
+++ tugasw3/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace tugasw3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelInput = new System.Windows.Forms.Label();
+            this.textBoxData = new System.Windows.Forms.TextBox();
+            this.btnProses = new System.Windows.Forms.Button();
+            this.labelBwh = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Location = new System.Drawing.Point(44, 50);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(105, 25);
+            this.labelInput.TabIndex = 0;
+            this.labelInput.Text = "Input Data :";
+            // 
+            // textBoxData
+            // 
+            this.textBoxData.Location = new System.Drawing.Point(51, 87);
+            this.textBoxData.Name = "textBoxData";
+            this.textBoxData.Size = new System.Drawing.Size(557, 31);
+            this.textBoxData.TabIndex = 1;
+            // 
+            // btnProses
+            // 
+            this.btnProses.Location = new System.Drawing.Point(51, 144);
+            this.btnProses.Name = "btnProses";
+            this.btnProses.Size = new System.Drawing.Size(111, 39);
+            this.btnProses.TabIndex = 2;
+            this.btnProses.Text = "Proses";
+            this.btnProses.UseVisualStyleBackColor = true;
+            this.btnProses.Click += new System.EventHandler(this.btnProses_Click);
+            // 
+            // labelBwh
+            // 
+            this.labelBwh.AutoSize = true;
+            this.labelBwh.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelBwh.Location = new System.Drawing.Point(51, 216);
+            this.labelBwh.Name = "labelBwh";
+            this.labelBwh.Size = new System.Drawing.Size(129, 41);
+            this.labelBwh.TabIndex = 3;
+            this.labelBwh.Text = "[EMPTY]";
+            this.labelBwh.Click += new System.EventHandler(this.labelBwh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 339);
+            this.Controls.Add(this.labelBwh);
+            this.Controls.Add(this.btnProses);
+            this.Controls.Add(this.textBoxData);
+            this.Controls.Add(this.labelInput);
+            this.Name = "Form1";
+            this.Text = "Form Pengaturan Warna";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.TextBox textBoxData;
+        private System.Windows.Forms.Button btnProses;
+        private System.Windows.Forms.Label labelBwh;
+    }
+}
+
---
+++ tugasw3/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tugasw3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void labelBwh_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProses_Click(object sender, EventArgs e)
+        {
+            var cekHuruf = StringComparison.OrdinalIgnoreCase;
+            var hitung = labelBwh.Font.Size;
+            if (textBoxData.Text.Substring(0,4).Equals("ISI:",cekHuruf) )
+            {
+                labelBwh.Text = textBoxData.Text.Substring(4);
+            }
+            else if (textBoxData.Text.Equals("SHOWN", cekHuruf))
+            {
+                labelBwh.Visible = true;
+            }
+            else if (textBoxData.Text.Equals("HIDE", cekHuruf))
+            {
+                labelBwh.Visible = false;
+            }
+            else if (textBoxData.Text.Equals("WARNA:DEFAULT", cekHuruf))
+            {
+                labelBwh.ForeColor = Color.Black;
+            }
+            else if (textBoxData.Text.Equals("WARNA:MERAH", cekHuruf))
+            {
+                labelBwh.ForeColor = Color.Red;
+            }
+            else if (textBoxData.Text.Equals("WARNA:HIJAU", cekHuruf))
+            {
+                labelBwh.ForeColor = Color.Green;
+            }
+            else if (textBoxData.Text.Equals("WARNA:BIRU", cekHuruf))
+            {
+                labelBwh.ForeColor = Color.Blue;
+            }
+            else if (textBoxData.Text.Equals("RESTART", cekHuruf))
+            {
+                labelBwh.ForeColor = Color.Black;
+                labelBwh.Font = new Font(labelBwh.Font.FontFamily, 15);
+                labelBwh.Text = "[EMPTY]";
+            }
+            else if (textBoxData.Text.Equals("BESARKAN", cekHuruf))
+            {
+                hitung++;
+                labelBwh.Font = new Font(labelBwh.Font.FontFamily, hitung);
+            }
+            else if (textBoxData.Text.Equals("kecilkan", cekHuruf))
+            {
+                hitung--;
+                labelBwh.Font = new Font(labelBwh.Font.FontFamily, hitung);
+            }
+            else
+            {
+                MessageBox.Show("Syntax tidak terdaftar!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
